1470da73dbf0ad4497eed5be03a6dfb273a2c1a7
Md Owais <84002244+md-owais@users.noreply.github.com>
2021-10-29 22:30:36 +0530

    Add files via upload

---
+++ 2.Addition of two numbers.py
@@ -0,0 +1,23 @@
+# Example-1:Addition of two numbers(Python3)
+"""
+num1 = 10
+num2 = 20
+# Adding two numbers
+num = num1 + num2
+# Printing values
+# print(num)
+print("Addition of {0} and {1} is {2}" .format(num1, num2, num))
+"""
+
+
+# Example-2: Adding two number provided by user input
+num1 = input(" Enter first number: ")
+num2 = input("\nEnter second number: ")
+
+# Adding two numbers
+# User might also enter float numbers
+num = float(num1) + float(num2)
+
+# Display the sum
+# will print value in float
+print("The sum of {0} and {1} is {2}".format(num1, num2, num))
